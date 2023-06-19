fa3c221d4ad95cfa1506b60456cc6cfc69571038
Jonas <sturmente@proton.me>
2023-06-19 16:56:01 +0200

    Initial commit

---
+++ src/main/java/de/sturmente/hardcore/Hardcore_plugin.java
@@ -0,0 +1,16 @@
+package de.sturmente.hardcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hardcore_plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
